d18eb4622f73727eac9d86ec4736ca3fc3091631
sai aditya chundi <saiaditya.chundi@gmail.com>
2019-02-28 01:54:03 +0530

    Some extensions for service messages

--- src/rospy_message_converter/json_message_converter.py
+++ src/rospy_message_converter/json_message_converter.py
@@ -12,13 +12,26 @@ def convert_json_to_ros_message(message_type, json_message):
         ros_message = convert_json_to_ros_message(message_type, json_message)
     """
     dictionary = json.loads(json_message)
+    
+    if dictionary.has_key("srv_name"):
+        del dictionary["srv_name"]
+    if dictionary.has_key("srv_type"):
+        del dictionary["srv_type"]
+    if dictionary.has_key("srv_kind"):
+        kind = dictionary["srv_kind"]
+        del dictionary["srv_kind"]
+        return message_converter.convert_dictionary_to_ros_message(message_type,\
+                dictionary, kind=kind)
+
     if dictionary.has_key("topic_name"):
-        del dictionary["topic"]
-    if dictionary.has_key("msg_typ"):
+        del dictionary["topic_name"]
+    if dictionary.has_key("msg_type"):
         del dictionary["msg_type"]
+
     return message_converter.convert_dictionary_to_ros_message(message_type, dictionary)
 
-def convert_ros_message_to_json(message, topic_name="", msg_type=""):
+def convert_ros_message_to_json(message, topic_name="", msg_type="",\
+        srv_name="", srv_type="", srv_kind=""):
     """
     Takes in a ROS message and returns a JSON-formatted string.
 
@@ -29,7 +42,13 @@ def convert_ros_message_to_json(message, topic_name="", msg_type=""):
     dictionary = message_converter.convert_ros_message_to_dictionary(message)
     if topic_name:
         dictionary["topic_name"]=topic_name
-    if msg_type:
-        dictionary["msg_type"]=msg_type
+        if msg_type:
+            dictionary["msg_type"]=msg_type
+    if srv_name:
+        dictionary["srv_name"]=srv_name
+        if srv_type:
+            dictionary["srv_type"]=srv_type
+        if srv_kind:
+            dictionary["srv_kind"]=srv_kind
     json_message = json.dumps(dictionary)
     return json_message
